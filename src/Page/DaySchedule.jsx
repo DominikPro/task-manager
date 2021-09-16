f2789bb51458f9ea1ce63bdc32a73ef2cdfe31d2
Dominik Proficz <dominik.proficz@gmail.com>
2021-09-17 00:41:55 +0200

    created  sortin task

--- src/Page/DaySchedule.jsx
+++ src/Page/DaySchedule.jsx
@@ -11,7 +11,7 @@ const DaySchedule = () => {
         <Row>
           <Col xs={12} >
             <Navbar style={{ backgroundColor: "#0F88F2", borderRadius: " 0px 0px 20px 20px" }} expand="lg" sticky="top" >
-            
+           
                   <h1 style={{ color: '#f7f7f7', fontSize: 18 }}>Challenges for today</h1>
  
                   <Sorting />
