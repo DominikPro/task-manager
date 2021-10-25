7b6052dccdb3c07d6a6faa246c929b8253a54d7e
Dominik Proficz <dominik.proficz@gmail.com>
2021-10-26 00:02:14 +0200

    adding gif instruction

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -7,22 +7,22 @@ import "./quote.css"
 const Quote = () => {
     const quotes = useSelector(state => state.quotesReducer)
     const [rundomQuoteNumber, setRundomQuoteNumber] = useState(0)
-    
+
     const drawAQuote = () => {
         setRundomQuoteNumber(Math.floor((Math.random() * 10)))
     }
 
     useEffect(() => {
         drawAQuote()
-    },[])
+    }, [])
 
     return (
-    <>
+        <>
             <div className="h50 mb-4">
                 <Row className="justify-content-center align-items-center">
 
                     <Col xs={12}  >
-                        <h3 className="text-center" >Hello!</h3>
+                        <h3 className="text-center" >Welcome! What's the plan for today?!</h3>
                     </Col>
 
                     <Col xs={12} md={8} className="mb-2  ">
@@ -31,7 +31,7 @@ const Quote = () => {
                             <Card.Header>Quote for you</Card.Header>
 
                             <Card.Body className="text-center ">
-                                
+
                                 <blockquote className="blockquote mb-0">
                                     <p>
                                         {quotes[rundomQuoteNumber].quotation}
@@ -40,19 +40,19 @@ const Quote = () => {
                                         Author <cite title="Source Title">{quotes[rundomQuoteNumber].author}</cite>
                                     </footer>
                                 </blockquote>
-                                
+
                                 <Button onClick={() => drawAQuote()} className="mt-2" variant="outline-info" > Next</Button>
-                                    
-                                </Card.Body>
-                                
+
+                            </Card.Body>
+
                         </Card>
                     </Col>
                 </Row>
             </div>
 
-     
 
-    </>
-)
+
+        </>
+    )
 }
 export default Quote;
